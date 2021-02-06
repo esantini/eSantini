3ff3a42eb56ecdbbee8a7728c53c29219f2050ca
Esteban Santini <esantinie@gmail.com>
2021-02-05 21:23:50 -0800

    Sense-HAT functionalities behind config switch

--- api/server.js
+++ api/server.js
@@ -1,7 +1,5 @@
 require('./init.js'); // Sets global.config from api/config.json && privateConfig.json
 const express = require('express');
-const senseLeds = require('sense-hat-led');
-const imu = require('node-sense-hat').Imu;
 const { addMessage, getMessage, init: dbInit } = require('./database');
 
 const IS_PROD = config.env === 'prod';
@@ -15,39 +13,45 @@ if (IS_PROD) {
   app.use(express.static('client/build'));
 }
 
-const IMU = new imu.IMU();
-app.get('/api/weather', (req, res) => {
-  //   const param = req.query.q;
-
-  IMU.getValue((error, data) => {
-    if (error !== null) {
-      console.error('Could not read sensor data: ', error);
-      return res.json({
-        msg: 'Could not read sensor data',
-        error,
-      });
-    }
-    console.log(new Date(), ' sending sensor data');
-    return res.json(data);
-  });
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
-});
-
 // LED message in sense-hat:
 let message = 'Hello World!';
 // cannot getMessage() before database is initialized so set message in dbInit(cb)
 dbInit(() => message = getMessage());
-if (senseLeds) {
-  startShowMessage();
-}
-function startShowMessage() {
+
+// disable sense-HAT in 'config.json'
+if (config.senseHatEnabled) {
+  const senseLeds = require('sense-hat-led');
+  const imu = require('node-sense-hat').Imu;
+  const IMU = new imu.IMU();
+  app.get('/api/weather', (req, res) => {
+    //   const param = req.query.q;
+
+    IMU.getValue((error, data) => {
+      if (error !== null) {
+        console.error('Could not read sensor data: ', error);
+        return res.json({
+          msg: 'Could not read sensor data',
+          error,
+        });
+      }
+      console.log(new Date(), ' sending sensor data');
+      return res.json(data);
+    });
+  });
+
   if (IS_PROD) {
-    senseLeds.showMessage(` ${message} `, 0.1, [255, 0, 0], startShowMessage);
+    startShowMessage();
+    function startShowMessage() {
+      senseLeds.showMessage(` ${message} `, 0.1, [255, 0, 0], startShowMessage);
+    }
   }
+} else {
+  app.get('/api/weather', (req, res) => res.json({ msg: "Sense-HAT not Available" }));
 }
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
 
 app.get('/api/message', (req, res) => res.json({ message }));
 app.post('/api/message', (req, res) => {
