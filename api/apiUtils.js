7cc4f0f891d55349ed09d153c2ffe36a53e25930
Esteban Santini <esantinie@gmail.com>
2021-02-16 03:29:59 -0800

    update self deploy steps

--- api/apiUtils.js
+++ api/apiUtils.js
@@ -2,20 +2,24 @@ const shell = require('shelljs');
 
 const err = 'Error: Git pull failed';
 
+const deployCommands = ['git pull', 'yarn', 'yarn build', 'pm2 restart server'];
+
 const update = (key, cb) => {
   if (key !== config.api_key) {
     shell.echo(`${err} (wrong key)`);
     return cb('Error: wrong key');
   }
 
-  const { code } = shell.exec('git pull');
-  if (code !== 0) {
-    shell.echo(`${err} code: ${code}`);
-    cb(`${err} code: ${code}`);
-  } else {
-    shell.echo('Git Pull Success');
-    cb();
+  for (const command of deployCommands) {
+    const { code } = shell.exec(command);
+    if (code !== 0) {
+      shell.echo(`${err} code: ${code}`);
+      return cb(`${err} code: ${code}`);
+    } else {
+      shell.echo(`${command} Success`);
+    }
   }
+  cb();
 };
 
 exports.update = update;
